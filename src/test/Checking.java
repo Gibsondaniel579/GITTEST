a45c7829458184516a7d928f0fb30b9d7f3dcfa4
Gibso <Gibso@Jeremiah>
2022-05-17 13:15:31 +0530

    This is my first commit...

---
+++ src/test/Checking.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Checking {
+	
+	@Test
+	public void testCaseOne()
+	{
+		System.out.println("Here, its my first testcase");
+	}
+
+}
